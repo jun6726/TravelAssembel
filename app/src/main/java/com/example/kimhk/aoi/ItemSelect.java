142fba53baa5537ff00ce60eb1708663c4b3654e
GitHub jun6726 <GitHub junmarko@naver.com>
2019-02-10 21:10:42 +0900

    2019.02.10 21:10

--- app/src/main/java/com/example/kimhk/aoi/ItemSelect.java
+++ app/src/main/java/com/example/kimhk/aoi/ItemSelect.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -23,11 +24,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -40,6 +44,11 @@ import java.util.Map;
 public class ItemSelect extends Activity {
     public int getData_position;
     TextView tv_position, tv_travel_date;
+    Button button;
+    ListView listView;
+    JSONArray Travels1 = null;
+
+    ArrayList<HashMap<String, String>> Array_list;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,7 +59,154 @@ public class ItemSelect extends Activity {
         getData_position = getData_Intent.getExtras().getInt("position");
         tv_position = (TextView) findViewById(R.id.tv_position);
         tv_travel_date = (TextView) findViewById(R.id.tv_travel_date);
+        button = (Button) findViewById(R.id.button);
+        listView = (ListView) findViewById(R.id.listVIew);
+        Array_list = new ArrayList<HashMap<String, String>>();
 
         tv_position.setText(getData_position + "번째 일정");
+
+        Position_send position_send = new Position_send();
+
+        position_send.execute("http://jun6726.cafe24.com/Item_select.php", String.valueOf(getData_position));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    getData("http://jun6726.cafe24.com/Item_select.php");
+            }
+        });
     }
+
+    public class Position_send extends AsyncTask<String, Void, String>
+    {
+        @Override
+        public String doInBackground(String... params) {
+
+            String URL = (String) params[0];
+            String position = (String) params[1];
+
+            Log.e("position", "Position!!!" + position);
+            try{
+                URL url = new URL(URL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(position.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+                InputStream inputStream;
+                inputStream = httpURLConnection.getInputStream();
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+                bufferedReader.close();
+                getData("http://jun6726.cafe24.com/Item_select.php");
+                return sb.toString();
+
+            } catch (ProtocolException e) {
+                Log.d("ProtocolException", "ProtocolException");
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                Log.d("MalformedURLException","MalformedURLException");
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.d("IOException","IOException");
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    String myJSON1;
+    private static final String TAG_RESULTS = "result";
+    private static final String TAG_Date = "Date";
+    private static final String TAG_Time="Time";
+    private static final String TAG_Cost="Cost";
+
+
+    public void getData(String url) {
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String uri = params[0];
+
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+
+                    return sb.toString().trim();
+
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+
+            @Override
+            public void onPostExecute(String result) {
+                myJSON1 = result;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute(url);
+    }
+
+    public void showList() {
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON1);
+            Travels1 = jsonObj.getJSONArray(TAG_RESULTS);
+
+            for (int i = 0; i < Travels1.length(); i++) {
+                JSONObject c = Travels1.getJSONObject(i);
+                String Date = c.getString(TAG_Date);
+                String Time = c.getString(TAG_Time);
+                String Cost = c.getString(TAG_Cost);
+
+                HashMap<String, String> persons = new HashMap<String, String>();
+
+                persons.put(TAG_Date, Date);
+                persons.put(TAG_Time, Time);
+                persons.put(TAG_Cost, Cost);
+
+                Array_list.add(persons);
+            }
+
+            ListAdapter adapter = new SimpleAdapter(
+                    ItemSelect.this, Array_list, R.layout.list_item1,
+                    new String[]{TAG_Date, TAG_Time, TAG_Cost},
+                    new int[]{R.id.Date, R.id.Time, R.id.Cost}
+            );
+            listView.setAdapter(adapter);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
